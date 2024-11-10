f0fc88c006d4e4948fb00727be5779df9be2fc88
Asteinnn <asteinwords1108@gmail.com>
2024-11-10 22:32:05 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+
+document.getElementById('Register').addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    
+    
+    alert('Logged In');
+    
+   
+    document.querySelector('form').reset();
+    
+    
+    window.location.href = "file:///C:/Users/aman1/Downloads/WEB/tourism.html";
+});
